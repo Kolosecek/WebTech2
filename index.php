c07b6a043de3ab18fa2fb29c7e59729f3266a421
Adam Trebichalský <dubst3pdnb@gmail.com>
2021-05-01 20:27:54 +0200

    styles added (sign in/sign up, profile page)

--- index.php
+++ index.php
@@ -18,46 +18,59 @@ if(!isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] !== true){
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Skúška</title>
+        <title>WEBTE2 - Skúška</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
         <link href='https://use.fontawesome.com/releases/v5.8.1/css/all.css'>
         <link href="styles.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Asap&display=swap" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://www.w3schools.com/lib/w3.js"></script>
     </head>
 
     <body>
-        <div class="titleWrap">
-            <h1 class="pageTitle">EXAM</h1>
-        </div>
-        <div class="container-lg">
-            <main class="form-signin">
-                <img class="mb-4" src="img.png" width="72" height="62">
-                <form method="GET" action="backend/controller_login.php" id="formToSend" enctype="multipart/form-data">
-                    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-                    <input style="display: none" name="type" type="text" value="login" class="form-control">
-                    <label for="inputEmail" class="visually-hidden">Email address</label>
-                    <input type="email" id="inputEmail" class="form-control" name="email" placeholder="Email address" required autofocus>
-                    <label for="inputPassword" class="visually-hidden">Password</label>
-                    <input type="password" id="inputPassword" class="form-control" name="password" placeholder="Password" required>
-                    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-                    <p>Don't have account? <a href="register.php">Sign up</a></p>
-                </form>
+        <div class="content">
+            <div id="illustration">
+                <img src="graphic.png" alt="" style=" width: 100%;">
+            </div>
+            <div id="login">
+                <main class="form-signin">
+                    <div class="forms">
+                        <form method="GET" action="backend/controller_login.php" id="formToSend" enctype="multipart/form-data">
+                            <div class="inputs">
+                                <img class="mb-4" src="img.png" width="72" height="62">
+                                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+                                <input style="display: none" name="type" type="text" value="login" class="form-control">
+                                <label for="inputEmail" class="visually-hidden">Email address</label>
+                                <input type="email" id="inputEmail" class="form-control" name="email" placeholder="Email address" required autofocus>
+                                <label for="inputPassword" class="visually-hidden">Password</label>
+                                <input type="password" id="inputPassword" class="form-control" name="password" placeholder="Password" required>
+                                <button class="w-100 btn btn-lg btn-primary grow" type="submit">Sign in</button>
+                                <p>Don't have account? <a href="register.php">Sign up</a></p>
+                            </div>
+                        </form>
+                    </div>
+                    <hr>
+                    <div class="forms">
+                        <form method="GET" action="backend/controller_exam.php" id="formToSend2" enctype="multipart/form-data">
+                            <div class="inputs">
+                                <img class="mb-4" src="img.png" width="72" height="62">
 
-                <img class="mb-4" src="img.png" width="72" height="62">
-                <form method="GET" action="backend/controller_exam.php" id="formToSend2" enctype="multipart/form-data">
-                    <h1 class="h3 mb-3 fw-normal">Connect to exam</h1>
-                    <input style="display: none" name="type" type="text" value="login" class="form-control">
-                    <label for="studentName" class="visually-hidden">Student Name</label>
-                    <input type="text" id="studentName" class="form-control" name="name" placeholder="Your Name" required autofocus>
-                    <label for="studentID" class="visually-hidden">Student ID</label>
-                    <input type="text" id="studentID" class="form-control" name="studentID" placeholder="Your Student ID" required>
-                    <label for="testID" class="visually-hidden">Exam ID</label>
-                    <input type="text" id="testID" class="form-control" name="testID" placeholder="Exam ID" required>
-                    <button class="w-100 btn btn-lg btn-primary" type="submit">Next</button>
-                </form>
-            </main>
+                                <h1 class="h3 mb-3 fw-normal">Connect to exam</h1>
+                                <input style="display: none" name="type" type="text" value="login" class="form-control">
+                                <label for="studentName" class="visually-hidden">Student Name</label>
+                                <input type="text" id="studentName" class="form-control" name="name" placeholder="Your Name" required autofocus>
+                                <label for="studentID" class="visually-hidden">Student ID</label>
+                                <input type="text" id="studentID" class="form-control" name="studentID" placeholder="Your Student ID" required>
+                                <label for="testID" class="visually-hidden">Exam ID</label>
+                                <input type="text" id="testID" class="form-control" name="testID" placeholder="Exam ID" required>
+                                <button class="w-100 btn btn-lg btn-primary grow" type="submit">Next</button>
+                            </div>
+                        </form>
+                    </div>
+                </main>
+            </div>
         </div>
 
         <script src="javascript/index.js"></script>
--- profile.php
+++ profile.php
@@ -29,19 +29,29 @@ $surname = $result[0]->getSurname();
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Online Exam - Profile</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     <link href='https://use.fontawesome.com/releases/v5.8.1/css/all.css'>
+    <link rel="stylesheet" href="styles.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://www.w3schools.com/lib/w3.js"></script>
 </head>
 <body>
-<h1>Hello <?= $name . " " . $surname . "!"?></h1>
-<button class="btn btn-primary" onclick="logout()">Log Out</button>
-<button class="btn btn-primary" onclick="newExam()">Create new exam</button>
-<button class="btn btn-primary" onclick="newQuestion()">Create new questions</button>
-<button class="btn btn-primary">List of already existing exams</button>
+<div class="content" style="justify-content: center; background: rgb(0,0,0);
+background: linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 100%);">
+        <img src="graphic.png" alt="" id="bg_blurred">
+    <div id="profile_title">
+        <h1>Hello <?= $name . " " . $surname . "!"?></h1>
+    </div>
+    <div id="profile_btns">
+        <button class="btn btn-primary btn-lg grow" style="width: auto !important;">List of already existing exams</button>
+        <button class="btn btn-primary btn-lg grow" style="width: auto !important;" onclick="newExam()">Create new exam</button>
+        <button class="btn btn-primary btn-lg grow" style="width: auto !important;" onclick="newQuestion()">Create new questions</button>
+        <button class="btn btn-primary btn-lg grow" style="width: auto !important;" onclick="logout()">Log Out</button>
+    </div>
+</div>
+
 </body>
 <script src="javascript/profile.js"></script>
 </html>
